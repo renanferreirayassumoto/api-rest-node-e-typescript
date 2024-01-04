8a620a3b575580b6c64bffd3e393482c4e2c447f
renanferreirayassumoto <renankosmo@gmail.com>
2024-01-04 17:54:51 -0400

    feat: jest config

--- tests/cidades/Create.test.ts
+++ tests/cidades/Create.test.ts
@@ -4,22 +4,40 @@ import {testServer} from '../jest.setup';
 describe('Cidades - Create', () => {
 
 	it('Cria registro', async () => {
-		const res1 = await testServer
+		const res = await testServer
 			.post('/cidades')
 			.send({nome: 'Campo Grande'});
 
-		expect(res1.statusCode).toEqual(StatusCodes.CREATED);
-		expect(typeof res1.body).toEqual('number');
+		expect(res.statusCode).toEqual(StatusCodes.CREATED);
+		expect(typeof res.body).toEqual('number');
 
 	});
 
 	it('Não pode criar um registro com nome muito curto', async () => {
-		const res1 = await testServer
+		const res = await testServer
 			.post('/cidades')
 			.send({nome: 'Ca'});
 
-		expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
-		expect(res1.body).toHaveProperty('errors.body.nome');
+		expect(res.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+		expect(res.body).toHaveProperty('errors.body.nome');
 
+	});
+
+	it('Não pode criar registro sem nome', async () => {
+		const res = await testServer
+			.post('/cidades')
+			.send({});
+
+		expect(res.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+		expect(res.body).toHaveProperty('errors.body.nome');
+	});
+
+	it('Não pode criar registro com nome que não é uma string', async () => {
+		const res = await testServer
+			.post('/cidades')
+			.send({nome: 123});
+
+		expect(res.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+		expect(res.body).toHaveProperty('errors.body.nome');
 	});
 });
