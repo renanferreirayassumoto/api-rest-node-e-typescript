0d3ae05eccedf1d8daa018416453479638dfbb26
renanferreirayassumoto <renankosmo@gmail.com>
2024-01-04 15:55:24 -0400

    feat: implement get all method

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -8,6 +8,7 @@ router.get('/', (_, res) => {
 	return res.send('Olá, DEV!');
 });
 
+router.get('/cidades',CidadesController.getAllValidation, CidadesController.getAll);
 router.post('/cidades',CidadesController.createValidation, CidadesController.create);
 
 export { router };
