3457c9827a806e45288cbd864e3dbf567cb0785e
renanferreirayassumoto <renankosmo@gmail.com>
2024-01-04 16:32:41 -0400

    feat: implemented all methods to "Cidades"

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-// import {StatusCodes} from 'http-status-codes';
 import {CidadesController} from './../controllers';
 
 const router = Router();
@@ -9,6 +8,9 @@ router.get('/', (_, res) => {
 });
 
 router.get('/cidades',CidadesController.getAllValidation, CidadesController.getAll);
+router.get('/cidades/:id',CidadesController.getByIdValidation, CidadesController.getById);
 router.post('/cidades',CidadesController.createValidation, CidadesController.create);
+router.put('/cidades/:id',CidadesController.updateByIdValidation, CidadesController.updateById);
+router.delete('/cidades/:id',CidadesController.deleteByIdValidation, CidadesController.deleteById);
 
 export { router };
