bd0a5c556623b5dd92ff0f5700dbd8be6f5a860d
renanferreirayassumoto <renankosmo@gmail.com>
2024-01-10 15:19:08 -0400

    feat: fix cors and implemented Swagger for routes

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import './shared/services/TranslationsYup';
 import { router } from './routes';
 
 const server = express();
+
+server.use(cors());
 
 server.use(express.json());
 
